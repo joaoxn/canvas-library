6d27326c4fdf52ecbc159f34e70e29162ae20590
João Olivo <jvso@yahoo.com>
2025-03-08 18:22:44 -0300

    Refactors OOP with new classes, Clickables deleted with UIElement's native support for click and type events,  more methods to classes, new game example

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 const canvas = document.getElementById("game") as HTMLCanvasElement;
 const ctx = canvas.getContext("2d")!;
 
+interface Shape {
+    x: number;
+    y: number;
+    hit(point: Vector): boolean;
+}
+
 class Vector {
     x: number;
     y: number;
@@ -23,9 +29,7 @@ class Vector {
     }
 
     insideCanvas(): boolean {
-        const rect = canvas.getBoundingClientRect();
-        const rectBox = new Box(rect.x, rect.y, rect.width, rect.height);
-        return rectBox.hit(new Vector(this.x, this.y))
+        return Box.fromHTML(canvas).hit(new Vector(this.x, this.y))
     }
 
     static add(vector1: Vector, vector2: Vector) {
@@ -46,7 +50,7 @@ class Vector {
     }
 }
 
-class Box {
+class Box implements Shape {
     x: number;
     y: number;
     width: number;
@@ -59,39 +63,66 @@ class Box {
         this.height = height;
     }
 
+    /**
+     * Creates a new Box instance from an HTML element.
+     * The Box's position and dimensions are calculated based on the element's position relative to the canvas.
+     *
+     * @param element - The HTML element to create the Box from.
+     * @returns A new Box instance representing the position and dimensions of the HTML element relative to the canvas.
+     */
+    static fromHTML(element: HTMLElement) {
+        const canvasRect = canvas.getBoundingClientRect();
+        const elementRect = element.getBoundingClientRect();
+
+        return new Box(elementRect.x - canvasRect.x, elementRect.y - canvasRect.y, 
+            elementRect.width, elementRect.height);
+    }
+
+
     hit(point: Vector): boolean {
         return point.x >= this.x && point.x <= this.x + this.width
             && point.y >= this.y && point.y <= this.y + this.height;
     }
 
-    inside(box: Box): number {
-        {
-            const left = this.x >= box.x;
-            const right = this.x + this.width <= box.x + box.width;
-            const top = this.y >= box.y;
-            const bottom = this.y + this.height <= box.y + box.height;
-
-            if (left && right && top && bottom)
-                return 1;
-        }
-
-        {
-            const left = this.x + this.width <= box.x;
-            const right = this.x >= box.x + box.width;
-            const top = this.y + this.height <= box.y;
-            const bottom = this.y >= box.y + box.height;
-
-            if (left && right && top && bottom)
-                return -1;
-        }
-
+    
+    /**
+     * Determines if this box is completely inside, completely outside, or intersects with another box.
+     *
+     * @param box - The box to check against.
+     * @param inclusive - If true, the function considers the bounding box of the box.
+     *                           If false, the function considers the area inside the box (exclusive).
+     *                           Default value is false.
+     *
+     * @returns 1 if this box is completely inside the given box, -1 if it is completely outside, and 0 if it intersects.
+     */
+    inside(box: Box, inclusive: boolean = false): number {
+        const compare = (a: number, b: number, isInclusive: boolean) => isInclusive ? a >= b : a > b;
+        const compareOpposite = (a: number, b: number, isInclusive: boolean) => isInclusive ? a <= b : a < b;
+    
+        const leftInside = compare(this.x, box.x, inclusive);
+        const rightInside = compareOpposite(this.x + this.width, box.x + box.width, inclusive);
+        const topInside = compare(this.y, box.y, inclusive);
+        const bottomInside = compareOpposite(this.y + this.height, box.y + box.height, inclusive);
+    
+        if (leftInside && rightInside && topInside && bottomInside) return 1;
+    
+        const leftOutside = compareOpposite(this.x + this.width, box.x, inclusive);
+        const rightOutside = compare(this.x, box.x + box.width, inclusive);
+        const topOutside = compareOpposite(this.y + this.height, box.y, inclusive);
+        const bottomOutside = compare(this.y, box.y + box.height, inclusive);
+    
+        if (leftOutside || rightOutside || topOutside || bottomOutside) return -1;
+    
         return 0;
-    }
+    }    
 
+    /**
+     * Determines if this box is completely inside, completely outside, or intersects with the canvas.
+     *
+     * @returns 1 if this box is completely inside the canvas, -1 if it is completely outside, and 0 if it intersects.
+     */
     insideCanvas(): number {
-        const rect = canvas.getBoundingClientRect();
-        const rectBox = new Box(rect.x, rect.y, rect.width, rect.height);
-        return this.inside(rectBox);
+        return this.inside(Box.fromHTML(canvas));
     }
 }
 
@@ -105,15 +136,17 @@ class Style {
 
 class UIElement extends Box {
     style: Style;
+    clickCallback?: (event: MouseEvent) => void;
+    keydownCallback?: (event: KeyboardEvent) => void;
 
     static elements: UIElement[] = [];
 
-    constructor(x: number, y: number, width: number, height: number, style?: Style) {
+    constructor(x: number, y: number, width: number, height: number, style: Style = new Style()) {
         super(x, y, width, height);
-        this.style = style ?? new Style();
+        this.style = style;
 
         UIElement.elements.push(this);
-        console.log(this, "was added to the list of drawed elements")
+        console.log(this, "was ADDED to the list of drawed elements")
     }
 
     draw() {
@@ -129,8 +162,6 @@ class UIElement extends Box {
 
             ctx.fillText(this.style.text, this.x, this.y + this.height - sizeDiff / 2, this.width);
         }
-
-
         // TODO: Implement more style features to the drawing
     }
 
@@ -138,49 +169,41 @@ class UIElement extends Box {
         for (const element of this.elements)
             element.draw();
     }
-}
 
-class Clickable extends UIElement {
-    callbackfn: (event: MouseEvent) => void;
-
-    static elements: Clickable[] = [];
-
-    constructor(x: number, y: number, width: number, height: number, callbackfn: (event: MouseEvent) => void, style?: Style) {
-        super(x, y, width, height, style);
-        this.callbackfn = callbackfn;
-
-        Clickable.elements.push(this);
-    }
-
-    static addListener() {
+    static addListeners() {
         document.addEventListener('click', (event) => {
             const rect = canvas.getBoundingClientRect();
-
-            if (!new Vector(event.x, event.y).insideCanvas()) return;
-
             const position = new Vector(event.x - rect.x, event.y - rect.y);
 
+            if (!position.insideCanvas()) return;
+
+
             for (const element of this.elements)
-                if (element.hit(position))
-                    element.callbackfn(event);
+                if (element.hit(position) && element.clickCallback)
+                    element.clickCallback(event);
+        })
+
+        document.addEventListener('keydown', (event) => {
+            for (const element of this.elements)
+                if (element.keydownCallback)
+                    element.keydownCallback(event);
         })
     }
 }
 
 class Movable extends UIElement {
-    velocity: Vector;
-    acceleration: Vector;
+    velocity: Vector = Vector.zero();
+    acceleration: Vector = Vector.zero();
     tickCallback?: (movable: Movable) => void;
 
     deleteIfOutOfBounds = false;
-    collisionGroup?: string;
+    collisionGroup: string | null = "default";
+    collisionCallback?: (movable: Movable, other: Movable) => void;
 
     static elements: Movable[] = [];
 
-    constructor(x: number, y: number, width: number, height: number, velocity?: Vector, acceleration?: Vector, style?: Style) {
+    constructor(x: number, y: number, width: number, height: number, style?: Style) {
         super(x, y, width, height, style);
-        this.velocity = velocity ?? Vector.zero();
-        this.acceleration = acceleration ?? Vector.zero();
 
         Movable.elements.push(this);
     }
@@ -192,48 +215,120 @@ class Movable extends UIElement {
             const idx = staticClass.elements.indexOf(this);
             staticClass.elements.splice(idx, 1);
         }
+        console.log(this, "was REMOVED from context")
+    }
+
+    collided(): Movable[] {
+        const collidedElements: Movable[] = [];
+        for (const other of Movable.elements) {
+            if (this !== other
+                && this.collisionGroup === other.collisionGroup
+                && this.inside(other) !== -1)
+                collidedElements.push(other);
+        }
+        return collidedElements;
+    }
+
+    tick() {
+        if (this.deleteIfOutOfBounds && this.insideCanvas() == -1) {
+            this.delete();
+            return;
+        }
+
+        if (this.collisionCallback) {
+            const collidedElements = this.collided();
+
+            for (const other of collidedElements) {
+                this.collisionCallback(this, other);
+                if (other.collisionCallback)
+                    other.collisionCallback(other, this);
+            }
+        }
+
+        if (this.tickCallback)
+            this.tickCallback(this);
+
+        this.x += this.velocity.x;
+        this.y += this.velocity.y;
+        this.velocity.add(this.acceleration);
     }
 
     static tick() {
-        for (const element of this.elements) {
-            if (element.deleteIfOutOfBounds && element.insideCanvas() == -1) {
-                element.delete();
-                continue;
-            }
-
-            if (element.tickCallback)
-                element.tickCallback(element);
-            element.x += element.velocity.x;
-            element.y += element.velocity.y;
-            element.velocity.add(element.acceleration);
-        }
+        for (const element of this.elements)
+            element.tick();
     }
 }
 
-Clickable.addListener();
+UIElement.addListeners();
 
-const square = new Movable(10, 10, 100, 100);
-square.velocity = Vector.one().scale(5);
+const square = new Movable(10, canvas.height - 100, 100, 100);
 
-square.tickCallback = (movable: Movable) => {
-    if (movable.x <= 0 || movable.x + movable.width >= canvas.width)
-        movable.velocity.x = -movable.velocity.x
-
-    if (movable.y <= 0 || movable.y + movable.height >= canvas.height)
-        movable.velocity.y = -movable.velocity.y
+square.keydownCallback = (event) => {
+    if (event.key !== ' ') return;
+    square.velocity.y = -20;
+    square.acceleration.y = 1;
 }
 
-// const button = new Clickable(canvas.width/2-100, canvas.height/2-50, 200, 100, (event) => console.log(event));
-// button.style.background = "#964B00";
-// button.style.text = "Play"
-// button.style.fontSize = 34;
+square.tickCallback = (movable) => {
+    if (movable.y + movable.height <= canvas.height) return
 
+    movable.y = canvas.height - movable.height;
+    movable.velocity.y = 0;
+    movable.acceleration.y = 0;
+}
+
+let frozen = false;
+
+class Obstacle extends Movable {
+    velocity = new Vector(-10, 0);
+    deleteIfOutOfBounds = true;
+
+    static defaultStyle = new Style();
+
+    static {
+        this.defaultStyle.background = "red";
+    }
+
+    constructor(width: number, height: number, style?: Style) {
+        style = style ?? Obstacle.defaultStyle;
+        super(canvas.width, canvas.height - height, width, height, style);
+    }
+
+    collisionCallback = (element: Movable, other: Movable) => {
+        if (other !== square) return;
+        frozen = true;
+    }
+}
+
+let reloadListener = document.addEventListener('keydown', (event) => {
+    if (frozen && event.key === ' ' || event.key === 'r') {
+        location.reload();
+    }
+})
+
+let i = 0;
 function loop() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    UIElement.drawAll();
-    Movable.tick();
 
+    UIElement.drawAll();
+    if (!frozen) {
+        Movable.tick();
+        if (i % 50 == 0)
+            new Obstacle(50, 80);
+    }
+
+    if (frozen) {
+        ctx.fillStyle = "#bbbbbb88";
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        ctx.fillStyle = "white";
+        ctx.font = "60px Arial";
+        ctx.fillText("Game Over", canvas.width / 2 - 150, canvas.height / 2);
+        ctx.font = "30px Arial";
+        ctx.fillText("Press R or Space to Restart", canvas.width / 2 - 200, canvas.height / 2 + 100);
+    }
+
+    i++;
     requestAnimationFrame(loop);
 }
 
